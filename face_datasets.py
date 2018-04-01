e65f921610d40a0372ca17e1aef87919f8c2a25e
Evan Pratten <ewpratten@gmail.com>
2018-04-01 17:03:36 -0400

    Add files via upload

---
+++ face_datasets.py
@@ -0,0 +1,69 @@
+import cv2
+import os
+import numpy as np
+import urllib.request
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    print("Setting file path...")
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print("Creating folder...")
+
+# Detect object in video stream using Haarcascade Frontal Face
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# For each person, one face id
+face_id = 2
+
+# Initialize sample face image
+count = 0
+
+assure_path_exists("./dataset/")
+
+# Start looping
+while(True):
+
+    # Capture video frame
+    with urllib.request.urlopen("http://172.16.10.241:8080/shot.jpg") as url:
+        imgReard = url.read()
+    imgNp = np.array(bytearray(imgReard), dtype=np.uint8)
+    img = cv2.imdecode(imgNp, -1)
+    img = cv2.resize(img, (900, 540))
+    image_frame = img
+
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect frames of different sizes, list of faces rectangles
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    # Loops for each faces
+    for (x,y,w,h) in faces:
+
+        # Crop the image frame into rectangle
+        cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
+
+        # Increment sample face image
+        count += 1
+
+        # Save the captured image into the datasets folder
+        cv2.imwrite("./dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+
+        # Display the video frame, with bounded rectangle on the person's face
+        cv2.imshow('frame', image_frame)
+    print("Count: " + str(count))
+    # To stop taking video, press 'q' for at least 100ms
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+
+    # If image taken reach 100, stop taking video
+
+    elif count>100:
+        break
+
+# Stop video
+
+
+# Close all started windows
+cv2.destroyAllWindows()
---
+++ face_recognition.py
@@ -0,0 +1,82 @@
+import urllib.request
+import cv2
+
+# Import numpy for matrices calculations
+import numpy as np
+
+import os 
+
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    print("Setting file path...")
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print("Creating folder...")
+
+# Create Local Binary Patterns Histograms for face recognization
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+assure_path_exists("trainer/")
+
+# Load the trained mode
+recognizer.read('trainer/trainer.yml')
+
+# Load prebuilt model for Frontal Face
+cascadePath = "haarcascade_frontalface_default.xml"
+
+# Create classifier from prebuilt model
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+# Set the font style
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+
+# Loop
+while True:
+    # Read the video frame
+    with urllib.request.urlopen("http://172.16.10.241:8080/shot.jpg") as url:
+        imgReard = url.read()
+    imgNp = np.array(bytearray(imgReard), dtype=np.uint8)
+    img = cv2.imdecode(imgNp, -1)
+    img = cv2.resize(img, (900, 540))
+    im = img
+
+    # Convert the captured frame into grayscale
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+    # Get all face from the video frame
+    faces = faceCascade.detectMultiScale(gray, 1.2,5)
+
+    # For each face in faces
+    for(x,y,w,h) in faces:
+
+        # Create rectangle around the face
+        cv2.rectangle(im, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
+
+        # Recognize the face belongs to which ID
+        Id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+
+        # Check the ID if exist 
+        if(Id == 1):
+            Id = "Evan {0:.2f}%".format(round(100 - confidence, 2))
+
+        if (Id == 2):
+            Id = "Warren {0:.2f}%".format(round(100 - confidence, 2))
+
+        # Put text describe who is in the picture
+        cv2.rectangle(im, (x-22,y-90), (x+w+22, y-22), (0,255,0), -1)
+        cv2.putText(im, str(Id), (x,y-40), font, 1, (255,255,255), 3)
+
+    # Display the video frame with the bounded rectangle
+    cv2.imshow('im',im) 
+
+    # If 'q' is pressed, close program
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+# Stop the camera
+cam.release()
+
+# Close all windows
+cv2.destroyAllWindows()
